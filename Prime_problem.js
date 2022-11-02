a8e2e5e40f224533706eff5fd21e3765c6db417f
Your Name <you@example.com>
2022-11-02 12:19:49 +0530

    code fixed

--- Prime_problem.js
+++ Prime_problem.js
@@ -1,12 +1,20 @@
-function check_prime(){
+function check_prime(num){
 	
-	for(let i=0 ; i<=num ; i++){
-		if(num%i){
-			return true;
-		}
-		return false;	
+	let count = 0;
+	for(let i=1 ; i<=num ; i++){
+		if(num%i == 0){
+			count++
+		}	
 	}
-}
-
-check_prime(13)
 	
+	if(count == 2){
+		return true;
+	}
+	return false;
+}
+let ans = check_prime(13)
+
+if(ans == true){
+console.log(num, "is prime");
+}else{
+console.log(num, "is not prime");	
